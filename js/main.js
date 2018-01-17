92a0f68c1bdfc757f04b050390ebda6b26a70069
Ankan <ankan.anurag@gmail.com>
2018-01-17 21:57:59 +0530

    Added accessibility options for image and keyboard navigation

--- js/main.js
+++ js/main.js
@@ -137,27 +137,33 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
+  let article = document.createElement('article');
+  article.tabIndex = 0;
+  li.append(article)
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  li.append(image);
+  image.alt = restaurant.alt_text;
+  console.log("restaurant alt: " + restaurant.alt_text);
+  article.append(image);
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  article.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  article.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  article.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.setAttribute("aria-label", "View Details for " + restaurant.name);
   li.append(more)
 
   return li
